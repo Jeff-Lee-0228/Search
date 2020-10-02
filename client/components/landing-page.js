0f278707951b23c932f2b12b7450f67fbde23d11
Jeff_Lee_0228 <suam3282@gmail.com>
2020-10-02 10:22:16 -0500

    feat: created landing-page

---
+++ client/components/landing-page.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+export default class LandingPage extends Component {
+  render() {
+    return <div> This is a Landing Page. </div>
+  }
+}
